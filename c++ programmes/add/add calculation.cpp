6b1b05b3af8994452b743a318fa27a603e758a01
M.Badar <44946634+MBadarr@users.noreply.github.com>
2025-02-21 11:26:25 +0500

    Update add calculation.cpp

--- c++ programmes/add/add calculation.cpp
+++ c++ programmes/add/add calculation.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 using namespace std;
 int main(int argc, char** argv) 
 {
